189e6982f069b8d2e44e82e08e0f95098dd94ec4
ILievILiyan <iliew.iliyan@gmail.com>
2023-04-30 11:04:57 +0300

    Update 01_calculator.py

--- 05_static_and_class_methods/lab/01_calculator.py
+++ 05_static_and_class_methods/lab/01_calculator.py
@@ -1,23 +1,42 @@
 from functools import reduce
-
+import operator
 
 class Calculator:
     @staticmethod
     def add(*args):
-        return reduce(lambda x,y: x + y, args)
+        return reduce(operator.add, args)
 
     @staticmethod
     def multiply(*args):
-        return reduce(lambda x,y: x * y, args)
+        return reduce(operator.mul, args)
 
 
     @staticmethod
     def divide(*args):
-        return reduce(lambda x,y: x / y, args)
+        return reduce(operator.truediv, args)
 
     @staticmethod
     def subtract(*args):
-        return reduce(lambda x,y: x - y, args)
+        return reduce(operator.sub, args)
+
+
+# class Calculator:
+#     @staticmethod
+#     def add(*args):
+#         return reduce(lambda x,y: x + y, args)
+#
+#     @staticmethod
+#     def multiply(*args):
+#         return reduce(lambda x,y: x * y, args)
+#
+#
+#     @staticmethod
+#     def divide(*args):
+#         return reduce(lambda x,y: x / y, args)
+#
+#     @staticmethod
+#     def subtract(*args):
+#         return reduce(lambda x,y: x - y, args)
 
 
 print(Calculator.add(5, 10, 4))
